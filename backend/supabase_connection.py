592a3a2279b0b0392aedc03e2e7613056da16c50
Ian Wong <ianwong1607@gmail.com>
2026-05-26 18:30:31 +0800

    fix: changed to use Supabase service role key instead of anon key for backend

--- backend/supabase_connection.py
+++ backend/supabase_connection.py
@@ -7,7 +7,7 @@ from dotenv import load_dotenv
 load_dotenv() 
 
 sb_url = os.environ.get("SUPABASE_URL")
-sb_anon_key = os.environ.get("SUPABASE_ANON_KEY")
+sb_service_role_key = os.environ.get("SUPABASE_SERVICE_ROLE_KEY")
 
 # Creates an instance of supabase client using create_client(url, key) 
-supabase_client = create_client(sb_url, sb_anon_key)
+supabase_client = create_client(sb_url, sb_service_role_key)
